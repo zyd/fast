7f4b9b3c1a9ff5f6b14fd06b8363df64d6b40afe
zhaoyidong <zhaoyidongzdd@foxmail.com>
2022-02-16 10:27:57 +0800

    firt commit

---
+++ src/test/java/ltd/edith/fast/FastApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.edith.fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
